8d08683ea4753154b98386fa8b4bad2eb2478760
nisseBD <70211737+nisseBD@users.noreply.github.com>
2020-09-23 16:29:40 +0200

    commit

--- Repitition/app.js
+++ Repitition/app.js
@@ -2,8 +2,30 @@ const express = require('express')
 const app = express()
 const port = 5555
 
+app.use(express.json())
+app.use(express.urlencoded())
+
+
 const clientDir = __dirname + "\\client\\"
 
 app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
+app.get('/bajs', (req, res) => {
+    res.sendFile(clientDir + 'main_style.css')
+})
+
+app.get('/korv', (req, res) => {
+  res.sendFile(clientDir + 'balle.jpg')
+})
+
+app.get('/great', (req, res) => {
+    res.sendFile(clientDir + 'success.html')
+  })
+
+
+app.post('/sendMessage', function (req, res) {
+    console.log(req.body.name)
+    console.log(req.body.email)
+  res.redirect("/great")
+})
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
