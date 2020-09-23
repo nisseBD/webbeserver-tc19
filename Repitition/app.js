8032af90c1aa62832e1985728c28e7898b25bf98
nisseBD <70211737+nisseBD@users.noreply.github.com>
2020-09-23 09:27:30 +0200

    commit

--- Repitition/app.js
+++ Repitition/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5555
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const clientDir = __dirname + "\\client\\"
+
+app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
